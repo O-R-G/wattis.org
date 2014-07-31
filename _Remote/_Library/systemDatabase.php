ee0a0adc54f54d61d1e91a7ec68a51e27fdd3e8b
David Reinfurt <reinfurt@o-r-g.com>
2014-07-31 10:39:54 -0400

    Add _Local, _Remote settings folders which include four files specific to each install, including two .htaccess hidden files

--- _Remote/_Library/systemDatabase.php
+++ _Remote/_Library/systemDatabase.php
@@ -8,10 +8,10 @@
 
 function systemDatabase() {
 
-	$dbMainHost = "localhost";
-	$dbMainUser = "root";
-	$dbMainPass = "";
-	$dbMainDbse = "wattis_local";
+	$dbMainHost = "vm-mysql-01";
+	$dbMainUser = "wattisdev";
+	$dbMainPass = "W4++15d3v";
+	$dbMainDbse = "wwwwattisdev";
 
 	$dbConnect = MYSQL_CONNECT($dbMainHost, $dbMainUser, $dbMainPass);
 	MYSQL_SELECT_DB($dbMainDbse, $dbConnect);
