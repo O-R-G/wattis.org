f1c95b1606b14629074efda1dbc90392834b8762
David Reinfurt <reinfurt@o-r-g.com>
2014-08-11 00:22:29 -0400

    Dev animatePunctuation.js

--- JS/animatePunctuation.js
+++ JS/animatePunctuation.js
@@ -18,87 +18,60 @@
         //    the other option is to save them out in an array, put in a marker,
         //    then write them back in the completed string after the fact
         //    either way it will require a lookaround, lookahead or some such
-        //
-        //    read up properly on regex
-        // http://stackoverflow.com/questions/406230/regular-expression-to-match-string-not-containing-a-word
+
+	// http://regex101.com/r/oY0bV5/1
+	// <[^@].*?>/gmi
+	// matches anything between < and >
+	// http://regex101.com/r/oY0bV5/3
+	// (<([^>]+)>)/ig
+	// ([<].*[>])?
+	// http://regex101.com/r/oY0bV5/4
+	// http://regex101.com/r/oY0bV5/2
+	// [,./][^h](?=e) looks ahead and matches ,./ and not an h when followed by e
+	
+	// seems like the way to do this is in two parts, first find the material in brackets, then put in placeholder text
+	// then search for punct, then replace. * not elegant *
+	// find any punctuation mark unless there is a "<" behind it or ">" in front of it
+	// problem seems to be no lookbehind in js
+
+	// unused regex
+
+        // re = /[!"#$%&()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞±≤≥¿¡«»–—“”‘’÷‹›¦−×⁏⁑‿⁀⁐ ∗∘∙∴∵≀∪∩⊂⊃┌┐]/g;
+	// re = /[,.]/g;	// minimal
+	// re = /<(.*?)>/gmi; 			// looser 
+	// re = /((>)(.*?)(<))/gmi; 
+	// re = /<([^>]+)\>/gmi; 
+	// re = /(<([^>]+)\>)/gi; 		// strict, with groups
+	// re = /(<)([.:,;])+?(>)/gi; 		// punct within < >
+	// re = /(?:<)([^>]+)(?:\>)/gmi; 	// non-capturing groups
+	// re = /([.:,;!]).+?(?!>)/gi; 		// punct within < > -- almost works
+
+	// three ways to tackle
+	
+	// 0. regex
+	// 1. js/dom
+	// 2. jQuery
 
 
-// http://regex101.com/r/oY0bV5/1
-// <[^@].*?>/gmi
-
-// matches anything between < and >
-// http://regex101.com/r/oY0bV5/3
-// (<([^>]+)>)/ig
-// ([<].*[>])?
-
-// http://regex101.com/r/oY0bV5/4
-
-// http://regex101.com/r/oY0bV5/2
-// [,./][^h](?=e) looks ahead and matches ,./ and not an h when followed by e
-
-// seems like the way to do this is in two parts, first find the material in brackets, then put in placeholder text
-// then search for punct, then replace. * not elegant *
-
-// find any punctuation mark unless there is a "<" behind it or ">" in front of it
-// problem seems to be no lookbehind in js
-
-
-// three ways to tackle
-
-// 0. regex
-// 1. js/dom
-// 2. jQuery
+// ALMOST HAVE THIS, ONLT THE MESSAGES[] ARRAY IS THE PROBLEM, OTHERWISE WORKING FOR INFINITE LEVEL DEEP
+// WITH EXCEPTION OF TOP LEVEL, BUT CAN CREATE A TEMP DIV THAT WRAPS ALL TO MAKE THAT ONE REPLACE CORRECTLY
 
 
         function initPunctuation(id, delay, animate) {
 
         	var messages = new Array();	
-		var divs = document.querySelectorAll("[id^=" + id + "]");
-		var delays = new Array();	
+		var divs = document.querySelectorAll("[id^=" + id + "]");	// better cross-browser method?
+		var delays = new Array();
 
 		//* find punctuation
 
 		for (var i = 0; i < divs.length; i++) {
 
-                        spanCount = 0;
-                        str = divs[i].innerHTML;
+			// callback issue, asynchronous, messages scope issue
 
-			// regex dev
-
-                        // re = /[!"#$%&()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞±≤≥¿¡«»–—“”‘’÷‹›¦−×⁏⁑‿⁀⁐ ∗∘∙∴∵≀∪∩⊂⊃┌┐]/g;
-                        // re = /[,.]/g;	// minimal
-                        // re = /["#$%&!()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞«»–—“”‘’]/g;		// works
-			
-			// var re = /<(.*?)>/gmi; 			// looser 
-			// var re = /((>)(.*?)(<))/gmi; 
-			// var re = /<([^>]+)\>/gmi; 
-			// var re = /(<([^>]+)\>)/gi; 		// strict, with groups
-			// var re = /(<)([.:,;])+?(>)/gi; 		// punct within < >
-			var re = /([.:,;!]).+?(?!>)/gi; 		// punct within < >
-			// var re = /(?:<)([^>]+)(?:\>)/gmi; 	// non-capturing groups
-			
-			// var str = "Mr Blue has a <blue> house and <a> blue car";
-			var str = "Hello, World! .> :> ,> ;>";
- 
-			var harvest = str.match(re);
-			messages[i] = harvest;
-
-			var result = str.replace(re, function(match1, match2, match3){
-
-				var replaced = "<span id='" + divs[i].id + "-" + spanCount + "' class='monaco big black'>" + match2 + "</span>";
-				// var replaced = "*" + match3 + "* ";
-
-				console.log(match1);
-				console.log(match2);
-				console.log(match3);
-				console.log(replaced);
-
-				spanCount++;
-				return replaced;
-			});
-
-                        divs[i].innerHTML = result;
+			findChildren(divs[i]);
 			delays[i] = delay;
+			messages[i] = ":";
 		}
 
 		// start animations
@@ -107,7 +80,7 @@
 
                 	for (var j = 0; j < divs.length; j++) {
 
-				animatePunctuation(divs[j],messages[j], delays[j]);
+				// animatePunctuation(divs[j],messages[j], delays[j]);
                 	}
 		}
 	}
@@ -122,7 +95,64 @@
 		}
 
 		messages.push(messages.shift());		// push / pop
-		var tt = setTimeout(function(){animatePunctuation(divs,messages,delays);}, delays); 	// surely something funny in how this sets delays as is passing an array (!) ** fix **
-		// var tt = setTimeout(function(){animatePunctuation(divs,messages,delays);}, delays[i]); 	// but this one doesnt work either
-		// refer to animatePunctuation logic to sort this out
+		var tt = setTimeout(function(){animatePunctuation(divs,messages,delays);}, delays); 	
 	}
+
+
+	function findChildren(thisDiv) {
+
+		// need to follow the pattern of first inventing a div to hold the divs so that 
+		// or maybe just the innerHTML of the top level div is enough
+		// see http://stackoverflow.com/questions/7864723/javascript-regexp-match-text-between-a-tags
+	
+		var children = thisDiv.children;
+		var messages = new Array();
+
+		if (thisDiv.children.length) {
+
+			console.log(thisDiv.children);
+			console.log("++++++++++");
+
+			for (j = 0; j < children.length; j++) {
+
+				findChildren(thisDiv.children[j]);
+			}
+
+		} else {
+
+			console.log("*");	
+			// replace
+
+			// spanCount not currently working	
+        		spanCount = 0;
+
+        		re = /["#$%&!()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞«»–—“”‘’]/g;
+			str = thisDiv.innerHTML;
+		
+		/*	messages scope issue
+
+			var harvest = str.match(re);	
+			for (k = 0; k < harvest.length; k++) {
+		
+				messages.push(harvest[k]);
+			}
+			console.log("messages = " + messages);
+			console.log("messages.length = " + messages.length);
+		*/		
+		
+			var result = str.replace(re, function(match1, match2, match3){
+		
+				var replaced = "<span id='" + thisDiv.id + "-" + spanCount + "' class='monaco big red'>" + match1 + "</span>";
+				spanCount++;
+				return replaced;
+			});
+		
+			// children[j].innerHTML = result;
+			thisDiv.innerHTML = result;
+		}	
+
+		// what to return?
+		// return thisDiv;
+		// return messages;		// ?
+	}
+
