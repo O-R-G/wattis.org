6fc7ea8cc7fb829e72a95d46b846bc1256c7683b
David Reinfurt <reinfurt@o-r-g.com>
2014-08-12 02:19:33 -0400

    dev animatePunctuation.js, working!

---
+++ JS/animatePunctuation-v3.js
@@ -0,0 +1,162 @@
+	// + todo	
+	//
+	//   - strip existing tags from the div before the search (or ignore them anyway via regex)
+	//     http://nadeausoftware.com/articles/2007/9/php_tip_how_strip_punctuation_characters_web_page
+	//     http://stackoverflow.com/questions/5233734/how-to-strip-punctuation-in-php
+	//   - if three punct are found next to each other, then that identfies a zone to animate
+	//   - second animation pattern for solo punctuation marks
+	//   - how many it takes to identify the animation could also be a parameter	
+
+
+        // stripping html tags:
+
+        // 0. search the input text for anything between '<' and '>'
+        // 1. remove these from the string that gets searched and replaced
+        //    though unsure how to both ignore them and then put them back in ...
+        //    maybe there is regex way to do this, i suspect there is with [^< >]
+        //    and that is surely the cleanest way
+        //    the other option is to save them out in an array, put in a marker,
+        //    then write them back in the completed string after the fact
+        //    either way it will require a lookaround, lookahead or some such
+
+	// http://regex101.com/r/oY0bV5/1
+	// <[^@].*?>/gmi
+	// matches anything between < and >
+	// http://regex101.com/r/oY0bV5/3
+	// (<([^>]+)>)/ig
+	// ([<].*[>])?
+	// http://regex101.com/r/oY0bV5/4
+	// http://regex101.com/r/oY0bV5/2
+	// [,./][^h](?=e) looks ahead and matches ,./ and not an h when followed by e
+	
+	// seems like the way to do this is in two parts, first find the material in brackets, then put in placeholder text
+	// then search for punct, then replace. * not elegant *
+	// find any punctuation mark unless there is a "<" behind it or ">" in front of it
+	// problem seems to be no lookbehind in js
+
+	// unused regex
+
+        // re = /[!"#$%&()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞±≤≥¿¡«»–—“”‘’÷‹›¦−×⁏⁑‿⁀⁐ ∗∘∙∴∵≀∪∩⊂⊃┌┐]/g;
+	// re = /[,.]/g;	// minimal
+	// re = /<(.*?)>/gmi; 			// looser 
+	// re = /((>)(.*?)(<))/gmi; 
+	// re = /<([^>]+)\>/gmi; 
+	// re = /(<([^>]+)\>)/gi; 		// strict, with groups
+	// re = /(<)([.:,;])+?(>)/gi; 		// punct within < >
+	// re = /(?:<)([^>]+)(?:\>)/gmi; 	// non-capturing groups
+	// re = /([.:,;!]).+?(?!>)/gi; 		// punct within < > -- almost works
+
+	// three ways to tackle
+	
+	// 0. regex
+	// 1. js/dom
+	// 2. jQuery
+
+
+// ALMOST HAVE THIS, ONLT THE MESSAGES[] ARRAY IS THE PROBLEM, OTHERWISE WORKING FOR INFINITE LEVEL DEEP
+// WITH EXCEPTION OF TOP LEVEL, BUT CAN CREATE A TEMP DIV THAT WRAPS ALL TO MAKE THAT ONE REPLACE CORRECTLY
+
+
+        function initPunctuation(id, delay, animate) {
+
+        	var messages = new Array();	
+		var divs = document.querySelectorAll("[id^=" + id + "]");	// better cross-browser method?
+		var delays = new Array();
+
+		//* find punctuation
+
+		for (var i = 0; i < divs.length; i++) {
+
+			// callback issue, asynchronous, messages scope issue
+
+			findChildren(divs[i]);
+			delays[i] = delay;
+			messages[i] = ":";
+		}
+
+		// start animations
+
+		if (animate) {
+
+                	for (var j = 0; j < divs.length; j++) {
+
+				// animatePunctuation(divs[j],messages[j], delays[j]);
+                	}
+		}
+	}
+
+
+        function animatePunctuation(divs, messages, delays) {
+
+		for (i = 0; i < messages.length; i++) {
+			
+			thisSpanId = divs.id + "-" + i;
+	   		document.getElementById(thisSpanId).innerHTML = messages[i];
+		}
+
+		messages.push(messages.shift());		// push / pop
+		var tt = setTimeout(function(){animatePunctuation(divs,messages,delays);}, delays); 	
+	}
+
+
+	function findChildren(thisDiv) {
+
+		// need to follow the pattern of first inventing a div to hold the divs so that 
+		// or maybe just the innerHTML of the top level div is enough
+		// see http://stackoverflow.com/questions/7864723/javascript-regexp-match-text-between-a-tags
+	
+		// separate the finding of children from replacing innerHTML
+		// get all children first, then replace
+		// at end, just replace the 
+
+		var children = thisDiv.children;
+		var messages = new Array();
+
+		if (thisDiv.children.length) {
+
+			console.log(thisDiv.children);
+			console.log("++++++++++");
+
+			for (j = 0; j < children.length; j++) {
+
+				findChildren(thisDiv.children[j]);
+			}
+
+		} else {
+
+			console.log("*");	
+			// replace
+
+			// spanCount not currently working	
+        		spanCount = 0;
+
+        		re = /["#$%&!()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞«»–—“”‘’]/g;
+			str = thisDiv.innerHTML;
+		
+		/*	messages scope issue
+
+			var harvest = str.match(re);	
+			for (k = 0; k < harvest.length; k++) {
+		
+				messages.push(harvest[k]);
+			}
+			console.log("messages = " + messages);
+			console.log("messages.length = " + messages.length);
+		*/		
+		
+			var result = str.replace(re, function(match1, match2, match3){
+		
+				var replaced = "<span id='" + thisDiv.id + "-" + spanCount + "' class='monaco big red'>" + match1 + "</span>";
+				spanCount++;
+				return replaced;
+			});
+		
+			// children[j].innerHTML = result;
+			thisDiv.innerHTML = result;
+		}	
+
+		// what to return?
+		// return thisDiv;
+		// return messages;		// ?
+	}
+
--- JS/animatePunctuation.js
+++ JS/animatePunctuation.js
@@ -1,78 +1,54 @@
-	// + todo	
 	//
-	//   - strip existing tags from the div before the search (or ignore them anyway via regex)
-	//     http://nadeausoftware.com/articles/2007/9/php_tip_how_strip_punctuation_characters_web_page
-	//     http://stackoverflow.com/questions/5233734/how-to-strip-punctuation-in-php
+	//   * in process *
+	//
+	//   - parse dom, replace punct
 	//   - if three punct are found next to each other, then that identfies a zone to animate
 	//   - second animation pattern for solo punctuation marks
 	//   - how many it takes to identify the animation could also be a parameter	
-
-
-        // stripping html tags:
-
-        // 0. search the input text for anything between '<' and '>'
-        // 1. remove these from the string that gets searched and replaced
-        //    though unsure how to both ignore them and then put them back in ...
-        //    maybe there is regex way to do this, i suspect there is with [^< >]
-        //    and that is surely the cleanest way
-        //    the other option is to save them out in an array, put in a marker,
-        //    then write them back in the completed string after the fact
-        //    either way it will require a lookaround, lookahead or some such
-
-	// http://regex101.com/r/oY0bV5/1
-	// <[^@].*?>/gmi
-	// matches anything between < and >
-	// http://regex101.com/r/oY0bV5/3
-	// (<([^>]+)>)/ig
-	// ([<].*[>])?
-	// http://regex101.com/r/oY0bV5/4
-	// http://regex101.com/r/oY0bV5/2
-	// [,./][^h](?=e) looks ahead and matches ,./ and not an h when followed by e
 	
-	// seems like the way to do this is in two parts, first find the material in brackets, then put in placeholder text
-	// then search for punct, then replace. * not elegant *
-	// find any punctuation mark unless there is a "<" behind it or ">" in front of it
-	// problem seems to be no lookbehind in js
-
-	// unused regex
-
-        // re = /[!"#$%&()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞±≤≥¿¡«»–—“”‘’÷‹›¦−×⁏⁑‿⁀⁐ ∗∘∙∴∵≀∪∩⊂⊃┌┐]/g;
-	// re = /[,.]/g;	// minimal
-	// re = /<(.*?)>/gmi; 			// looser 
-	// re = /((>)(.*?)(<))/gmi; 
-	// re = /<([^>]+)\>/gmi; 
-	// re = /(<([^>]+)\>)/gi; 		// strict, with groups
-	// re = /(<)([.:,;])+?(>)/gi; 		// punct within < >
-	// re = /(?:<)([^>]+)(?:\>)/gmi; 	// non-capturing groups
-	// re = /([.:,;!]).+?(?!>)/gi; 		// punct within < > -- almost works
-
-	// three ways to tackle
-	
-	// 0. regex
-	// 1. js/dom
-	// 2. jQuery
-
-
-// ALMOST HAVE THIS, ONLT THE MESSAGES[] ARRAY IS THE PROBLEM, OTHERWISE WORKING FOR INFINITE LEVEL DEEP
-// WITH EXCEPTION OF TOP LEVEL, BUT CAN CREATE A TEMP DIV THAT WRAPS ALL TO MAKE THAT ONE REPLACE CORRECTLY
+	// http://regex101.com/r/yT7gZ0/1#javascript
+	// ([.,;:])(?![^<]*>|[^<>]*<\/)
 
 
         function initPunctuation(id, delay, animate) {
 
-        	var messages = new Array();	
 		var divs = document.querySelectorAll("[id^=" + id + "]");	// better cross-browser method?
-		var delays = new Array();
+										// fix to make by class name
+		// fix naming of arrays to more meaningful/consistent
 
+        	var punctdivs = new Array();	
+		var delays = new Array();
+		var punct = new Array();
+		var puncts = new Array();
+
+                // var re = /["#$%&!()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞«»–—“”‘’]/g;			// these two should match
+
+		// find all text not between tags (ie, a child) or in tag
+		var re = /([*+.:,;!?\(\)\[\]\{\}~°•“”‘’–—])(?![^<]*>|[^<>]*<\/)/g; 			// fill out punct
+	
 		//* find punctuation
 
 		for (var i = 0; i < divs.length; i++) {
+			
+			replaceChildren(divs[i]);
+			divs[i].innerHTML = divs[i].innerHTML.replace(re, "<span class='punctuation'>$1</span>");
 
-			// callback issue, asynchronous, messages scope issue
-
-			findChildren(divs[i]);
 			delays[i] = delay;
-			messages[i] = ":";
+			
+			// naming of punct, punctdiv etc  ??
+
+			punctdiv = divs[i].getElementsByClassName('punctuation');
+			punctdivs.push(punctdiv);
+
+                        for (var k = 0; k < punctdiv.length; k++) {
+		
+				punct.push(punctdivs[0][k].innerHTML);
+				console.log(k + "> " + punctdivs[0][k].innerHTML);
+			}	
+
+			puncts.push(punct);
 		}
+
 
 		// start animations
 
@@ -80,79 +56,43 @@
 
                 	for (var j = 0; j < divs.length; j++) {
 
-				// animatePunctuation(divs[j],messages[j], delays[j]);
+				animatePunctuation(divs[j],punctdivs[j],puncts[j],delays[j]);
                 	}
 		}
 	}
 
 
-        function animatePunctuation(divs, messages, delays) {
 
-		for (i = 0; i < messages.length; i++) {
-			
-			thisSpanId = divs.id + "-" + i;
-	   		document.getElementById(thisSpanId).innerHTML = messages[i];
-		}
+	// this could be written inline to the above function
 
-		messages.push(messages.shift());		// push / pop
-		var tt = setTimeout(function(){animatePunctuation(divs,messages,delays);}, delays); 	
-	}
+	function replaceChildren(thisDiv) {
 
-
-	function findChildren(thisDiv) {
-
-		// need to follow the pattern of first inventing a div to hold the divs so that 
-		// or maybe just the innerHTML of the top level div is enough
-		// see http://stackoverflow.com/questions/7864723/javascript-regexp-match-text-between-a-tags
-	
-		var children = thisDiv.children;
-		var messages = new Array();
+		// this should match the other regex and could be passed as well
+		var re = /(["#$%&!()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞«»–—“”‘’])/g;		
 
 		if (thisDiv.children.length) {
 
-			console.log(thisDiv.children);
-			console.log("++++++++++");
+			for (j = 0; j < thisDiv.children.length; j++) {
 
-			for (j = 0; j < children.length; j++) {
-
-				findChildren(thisDiv.children[j]);
+				replaceChildren(thisDiv.children[j]);
 			}
-
 		} else {
 
-			console.log("*");	
-			// replace
-
-			// spanCount not currently working	
-        		spanCount = 0;
-
-        		re = /["#$%&!()*+,\-.\/:;<=>?@\[\\\]^_`\{|\}~°•´∞«»–—“”‘’]/g;
-			str = thisDiv.innerHTML;
-		
-		/*	messages scope issue
-
-			var harvest = str.match(re);	
-			for (k = 0; k < harvest.length; k++) {
-		
-				messages.push(harvest[k]);
-			}
-			console.log("messages = " + messages);
-			console.log("messages.length = " + messages.length);
-		*/		
-		
-			var result = str.replace(re, function(match1, match2, match3){
-		
-				var replaced = "<span id='" + thisDiv.id + "-" + spanCount + "' class='monaco big red'>" + match1 + "</span>";
-				spanCount++;
-				return replaced;
-			});
-		
-			// children[j].innerHTML = result;
-			thisDiv.innerHTML = result;
+			thisDiv.innerHTML = thisDiv.innerHTML.replace(re, "<span class='punctuation'>$1</span>");
 		}	
+		return true;
+	}
 
-		// what to return?
-		// return thisDiv;
-		// return messages;		// ?
+
+
+        function animatePunctuation(divs, punctdivs, puncts, delays) {
+
+		for (i = 0; i < punctdivs.length; i++) {
+
+	   		punctdivs[i].innerHTML = puncts[i];
+		}
+
+    		puncts.push(puncts.shift());
+		var tt = setTimeout(function(){animatePunctuation(divs,punctdivs,puncts,delays);}, delays); 	
 	}
 
