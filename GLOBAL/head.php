7bfc704d5679b2cdf19502d53a933137d00b77bb
David Reinfurt <reinfurt@o-r-g.com>
2014-09-18 16:53:21 -0400

    add _Outbox/_Template.zip, speedControl in dev

--- GLOBAL/head.php
+++ GLOBAL/head.php
@@ -40,9 +40,7 @@
 <body>
 
 <!-- Google Tag Manager -->
-<!--
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N8MCCT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N8MCCT');</script>
--->
 <!-- End Google Tag Manager -->
 
 
@@ -66,8 +64,9 @@ if
 ?>
 	
 	<div class="logoContainer times big logo fixed">
-	<span id="logo" class="target">.+*</span>
-	<span id="control" onmousedown="startStopAnimatePunctuation();" class="helvetica small">CONTROL</span>
+	<span id="logo" onmousedown="startStopAnimatePunctuation();" class="target">.+*</span>
+	<!-- <span id="logo" class="target">.+*</span> -->
+	<!-- <span id="control" onmousedown="startStopAnimatePunctuation();" class="helvetica small">CONTROL</span> -->
 	<a href="<?php echo ($pageName == 'index') ? 'main' : 'index?alt=1'; ?>" style="color:#000;">The Wattis Institute</a>
 	</div>
 	
