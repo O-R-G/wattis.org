83ba5b245a95fec88d171f174e61931ab477375d
lily healey <lily@lilyhealey.co.uk>
2016-03-17 11:17:10 -0400

    start on our mind page (inline imgs)

--- JS/animateNewsTicker.js
+++ JS/animateNewsTicker.js
@@ -1,35 +1,31 @@
-	// globals
-	// requires newsItem[] set elsewhere
+// globals
+// requires newsItem[] set elsewhere
+newsCounter = 0;
 
-	newsCounter = 0;
+function animateNewsTicker(thisnewsItem)
+{	
+	if(thisnewsItem == null)
+	{ 
+		thisNewsItem = "Sorry, no news *now.*";
+	}
 
-        function animateNewsTicker(thisnewsItem) {	
+	// rewrite innerhtml
+	thisinnerhtml = "<span id='news-item'>";
+	thisinnerhtml += thisnewsItem;
+	thisinnerhtml += "</span>";
+	document.getElementById("news").innerHTML = thisinnerhtml;
 
-		if (thisnewsItem == null) { 
+	// increment pointer 
+	if(newsCounter >= newsItem.length - 1) 
+	{
+		newsCounter = 0;
+	} 
+	else 
+	{	
+		newsCounter++;
+	}
 
-			thisNewsItem = "Sorry, no news *now.*";
-		}
-
-		// rewrite innerhtml
-
-		thisinnerhtml = "<span id='news-item'>";
-		thisinnerhtml += thisnewsItem;
-		thisinnerhtml += "</span>";
-		document.getElementById("news").innerHTML = thisinnerhtml;
-
-		// increment pointer 
-
-		if ( newsCounter >= newsItem.length - 1 ) {
-
-			newsCounter=0;
-
-		} else {
-		
-			newsCounter++;
-		}
-
-		// set timeout
-		// scope issue *fix*
-
-		var tt = setTimeout("animateNewsTicker(newsItem[newsCounter])", 3000);
- 	}
+	// set timeout
+	// scope issue *fix*
+	var tt = setTimeout("animateNewsTicker(newsItem[newsCounter])", 3000);
+}
--- index.php
+++ index.php
@@ -2,65 +2,69 @@
 require_once("GLOBAL/head.php");
 require_once("_Library/orgRSSParse.php");
 ?>
-
-
 <!-- BLOCKS -->
+<div class="homeContainer times big"><?
 
-<div class="homeContainer times big">
+$rootname = 'Home';
 
-	<?php
+// SQL objects attached to root by name
+$sql = "SELECT 
+			objects.id AS objectsId, 
+			objects.name1, objects.body, objects.url, 
+			objects.begin, objects.end 
+		FROM 
+			objects, wires 
+		WHERE 
+			wires.fromid = (
+				SELECT objects.id 
+				FROM objects 
+				WHERE 
+					objects.name1 LIKE '$rootname' 
+					AND objects.active = 1
+			) 
+			AND objects.name1 NOT LIKE '.%' 
+			AND wires.toid = objects.id 
+			AND objects.active = '1' 
+			AND wires.active = '1' 
+		ORDER BY objects.rank;";
 
-	$rootname = 'Home';
+$result = MYSQL_QUERY($sql);
+$html = "";
+$i = 0;
+$myrow = MYSQL_FETCH_ARRAY($result);
+$blocks[$i] = "<div class = 'logoContainer'>" . $myrow["body"] . "</div>";
 
-        // SQL objects attached to root by name
+while($myrow =  MYSQL_FETCH_ARRAY($result)) 
+{
+	if(!$isMobile)
+	{
+		$randomPadding = rand(0, 150);
+		$randomWidth = rand(10, 35);
+		$randomMargin = rand(30, 80);
+		$randomFloat = (rand(0, 1) == 0) ? 'left' : 'right';
+	}
+	else
+	{
+		$randomPadding = rand(0, 30);
+		$randomWidth = rand(30, 80);
+		$randomMargin = rand(20, 50);
+		$randomFloat = (rand(0, 1) == 0) ? 'left' : 'right';
+	}
+	$blocks[$i] .= "<div class = 'blockContainer' style='width:".$randomWidth."%; float: ".$randomFloat."; padding-top:".$randomPadding."px; margin: ".$randomMargin."px;'>";
+	$blocks[$i] .= $myrow["body"];
+	$blocks[$i] .= "</div>";
 
-        $sql = "SELECT objects.id AS objectsId, objects.name1, objects.body, objects.url, 
-objects.begin, objects.end FROM objects, wires WHERE wires.fromid=(SELECT objects.id FROM objects 
-WHERE objects.name1 LIKE '$rootname' AND objects.active=1) AND objects.name1 NOT LIKE '.%' AND wires.toid = objects.id AND 
-objects.active = '1' AND wires.active = '1' ORDER BY objects.rank;";
+	$i++;
+}
 
-        $result = MYSQL_QUERY($sql);
-        $html = "";
-        $i = 0;
+// write blocks
+for ($j = 0; $j < count($blocks); $j++)
+{
+	$html .= $blocks[$j];
+}
+echo nl2br($html);
 
-        $myrow = MYSQL_FETCH_ARRAY($result);
-	$blocks[$i] = "<div class = 'logoContainer'>" . $myrow["body"] . "</div>";
-
-        while ( $myrow  =  MYSQL_FETCH_ARRAY($result) ) {
-				if(!$isMobile)
-				{
-					$randomPadding = rand(0, 150);
-					$randomWidth = rand(10, 35);
-					$randomMargin = rand(30, 80);
-					$randomFloat = (rand(0, 1) == 0) ? 'left' : 'right';
-                }
-                else
-                {
-                	$randomPadding = rand(0, 30);
-					$randomWidth = rand(30, 80);
-					$randomMargin = rand(20, 50);
-					$randomFloat = (rand(0, 1) == 0) ? 'left' : 'right';
-                }
-                $blocks[$i] .= "<div class = 'blockContainer' style='width:" . $randomWidth . "%; float:" . $randomFloat . "; padding-top:" . $randomPadding . "px; margin: " . $randomMargin . "px;'>";
-                $blocks[$i] .= $myrow["body"];
-                $blocks[$i] .= "</div>";
-
-                $i++;
-        }
-
-	// write blocks
-
-        for ( $j = 0; $j < count($blocks); $j++) {
-
-                $html .= $blocks[$j];
-        }
-
-	echo nl2br($html);
-
-	?>
-        
-</div>
-
+?></div>
 
 <!-- WEATHER -->
 <script type="text/javascript">
@@ -72,9 +76,7 @@ if(!!el)
 }
 </script>
 
-
 <!-- NEWS -->
-
 <script type="text/javascript" src="JS/animateNewsTicker.js"></script>
 <script type="text/javascript">
 
