5ba6ba91a1fb730716b8c7c6ce356c3e85614dd2
david reinfurt <reinfurt@o-r-g.com>
2019-11-08 11:23:52 -0500

    library_ in process

--- library_.php
+++ library_.php
@@ -6,13 +6,54 @@ require_once("GLOBAL/head.php");
 
 	<?php
 
-        $rootid = $ids[0];
+        // get Library base_id
+
+        $sql = "SELECT objects.id, objects.name1 FROM objects WHERE objects.name1 LIKE 'Library%' AND objects.active=1 LIMIT 1;";
+        $result = MYSQL_QUERY($sql);
+        while ($myrow = MYSQL_FETCH_ARRAY($result)) {
+            $base_id = $myrow['id'];
+            $base_name = $myrow['name1'];
+        }
+
+
+    // build submenu
+
+    $sql = "SELECT objects.id, objects.name1 FROM objects, wires WHERE wires.fromid = $base_id AND objects.active = 1 
+AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";         
+    $result = MYSQL_QUERY($sql);
+    while ($myrow = MYSQL_FETCH_ARRAY($result)) {
+        // default to first submenu item
+        if (!$count)
+            $submenu_selected_id = $myrow['id'];
+        // ** not yet working **
+        if ($myrow['id'] == $id)
+            $html_submenu .= $myrow['name1'] . "<br/>";
+        else
+            $html_submenu .= "<a href='library_.php?id=" . $myrow['id'] . "'>" . $myrow['name1'] ."</a><br/>";
+        $count++;
+    }
+    
+
+    // build categories
+
+    // $submenu_selected_id = $myrow['id'];
+    $sql = "SELECT objects.id, objects.name1 FROM objects, wires WHERE wires.fromid = $submenu_selected_id AND objects.active = 1 
+AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";
+    $result = MYSQL_QUERY($sql);
+    while ($myrow = MYSQL_FETCH_ARRAY($result))
+        $categories[] = $myrow['name1'];
+
+
+
+        // $root_id = $ids[0];
+        $root_id = 757;          // ** fix **
+        $id = 757;              // ** fix **
 
         // SQL objects attached to object plus media plus rootname, rootbody
 
     	$sql = "SELECT objects.id AS objectsId, objects.name1, objects.deck, objects.body, objects.rank, (SELECT 
-objects.name1 FROM objects WHERE objects.id = $rootid) AS rootname, (SELECT objects.body FROM objects WHERE objects.id = 
-$rootid) AS rootbody, wires.fromid, wires.toid, media.id AS mediaId, media.object, media.caption, media.type, media.active 
+objects.name1 FROM objects WHERE objects.id = $root_id) AS rootname, (SELECT objects.body FROM objects WHERE objects.id = 
+$root_id) AS rootbody, wires.fromid, wires.toid, media.id AS mediaId, media.object, media.caption, media.type, media.active 
 AS mediaActive FROM wires, objects LEFT JOIN media ON objects.id = media.object AND media.active = 1 WHERE wires.fromid = 
 (SELECT objects.id FROM objects WHERE objects.id = $id AND objects.active = 1) AND wires.toid=objects.id AND wires.active = 
 1 ORDER BY objects.rank;";
@@ -42,7 +83,7 @@ AS mediaActive FROM wires, objects LEFT JOIN media ON objects.id = media.object 
 				$use4xgrid = ($rootname == "Buy Catalogs") ? TRUE : FALSE;		       
 	                }
 
-			$images[$i] .= "<a href='buy_.php?id=" . $rootid . "," . $myrow['objectsId'] . "'>";
+			$images[$i] .= "<a href='buy_.php?id=" . $root_id . "," . $myrow['objectsId'] . "'>";
 			$images[$i] .= "<div id='image".$i."' class = 'listContainer " . (($use4xgrid) ? "fourcolumn" : "twocolumn") . "'>";
 			$images[$i] .= displayMedia($mediaFile, $mediaCaption, $mediaStyle);
 			$images[$i] .= "<div class = 'captionContainer helvetica small'>";
@@ -56,22 +97,22 @@ AS mediaActive FROM wires, objects LEFT JOIN media ON objects.id = media.object 
 		}
 	}
 
-    // build submenu
 
-    $thisid = $ids[0];
-    $sql = "SELECT objects.id, objects.name1 FROM objects, wires WHERE wires.fromid = $thisid AND objects.active = 1 
-AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";         
-    $result = MYSQL_QUERY($sql);
-    while ($myrow = MYSQL_FETCH_ARRAY($result))
-        $submenu .= "<a href='library_.php?id=" . $myrow['id'] . "'>" . $myrow['name1'] ."</a><br/>";
+
+
+
+
+
+
+    // ------ $html -------
 
 	// name
 
 	$html .= "<div class='listContainer times'>";
-    $html .= $rootname;
+    $html .= $base_name;
 	$html .= "<br /><br />" . $rootbody .  "<br /><br />";
-    if ($submenu)
-    	$html .= $submenu;
+    if ($html_submenu)
+    	$html .= $html_submenu;
 	$html .= "</div>";	
 
 
