ac49ffddc27eec487d5554ccbf2c8beb7e502a83
david reinfurt <reinfurt@o-r-g.com>
2020-03-20 15:13:00 -0400

    fix library search to work across all categories

--- library_.php
+++ library_.php
@@ -35,20 +35,39 @@ require_once("GLOBAL/head.php");
                 $html_submenu .= "<a href='library_.php?id=" . $base_id . "," . $s['id'] . "'>" . "<button class = 'helvetica small'>" . $s['name'] ."</button></a>";
         $html_submenu .= '</div>';
         if ($search)
-            $html_submenu = "<br/>Search: <i>$search</i><br><span id = 'search_count'></span> results.";
+            $html_submenu = "<br/>Search: <i>$search</i><br><span id = 'search_count'></span> matches...";
 
-        // build categories
+        if ($search) {
 
-        $sql = "SELECT objects.id, objects.name1 FROM objects, wires WHERE wires.fromid = $submenu_id AND objects.active = 1 
-                AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";
-        $result = MYSQL_QUERY($sql);
-        $count = 0;
-        while ($myrow = MYSQL_FETCH_ARRAY($result)) {
-            $categories[$count]['id'] = $myrow['id'];
-            $categories[$count]['name'] = $myrow['name1'];
-            $count++;
+            // build all categories (search)
+    
+            $count = 0;
+            foreach($submenu as $s) {
+                $s_id = $s['id'];
+                $sql = "SELECT objects.id, objects.name1 FROM objects, wires WHERE wires.fromid = $s_id AND objects.active = 1 
+                        AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";
+                $result = MYSQL_QUERY($sql);
+                while ($myrow = MYSQL_FETCH_ARRAY($result)) {
+                    $categories_search[$count]['id'] = $myrow['id'];
+                    $categories_search[$count]['name'] = $myrow['name1'];
+                    $count++;
+               }
+            }
+
+        } else {
+
+            // build categories (this page)
+    
+            $count = 0;
+            $sql = "SELECT objects.id, objects.name1 FROM objects, wires WHERE wires.fromid = $submenu_id AND objects.active = 1 
+                    AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";
+            $result = MYSQL_QUERY($sql);
+            while ($myrow = MYSQL_FETCH_ARRAY($result)) {
+                $categories[$count]['id'] = $myrow['id'];
+                $categories[$count]['name'] = $myrow['name1'];
+                $count++;
+            }
         }
- 
             
         // build search
 
@@ -58,17 +77,14 @@ require_once("GLOBAL/head.php");
             currently passes $base_id and $sub_id as hidden value in form
         */
 
-        if (!$search) {                                
-            $search_id = $base_id . "," . $sub_id;
-            $html_search  = "<div id='library-search-container'>";
-            $html_search .= "<form action='library_.php'>";
-            $html_search .= "<input id='library-search-field' type='text' placeholder='Search the Library ...' name='search'>";
-            $html_search .= "<input type='hidden' id='id' name='search_id' value=$search_id>";
-            $html_search .= "<button type='submit'><img id='library-search-icon' src='MEDIA/svg/magnifying-glass-6-k.svg'></button>";
-            $html_search .= "</form>";
-            $html_search .= "</div>";
-        } 
-
+        $search_id = $base_id . "," . $sub_id;
+        $html_search  = "<div id='library-search-container'>";
+        $html_search .= "<form action='library_.php'>";
+        $html_search .= "<input id='library-search-field' type='text' placeholder='Search The Wattis Library ...' name='search'>";
+        $html_search .= "<input type='hidden' id='id' name='search_id' value=$search_id>";
+        $html_search .= "<button type='submit'><img id='library-search-icon' src='MEDIA/svg/magnifying-glass-6-k.svg'></button>";
+        $html_search .= "</form>";
+        $html_search .= "</div>";
         
         // output $html
     
@@ -95,55 +111,31 @@ require_once("GLOBAL/head.php");
 	    $html .= "</div>";	
 	    echo nl2br($html);
         $html = "";
-
-        
+       
         // build objects per category
 
-        foreach ($categories as $c) {
-
-            if ($search) {
+        if ($search) {
     
-            // SQL objects where name1 like $search and connected to Library object plus media
+            // pulls from all 4 categories attached to library
+            // matches on $categories_search[]['id']
 
-            /*   
-    	    $sql = "SELECT objects.id AS objectsId, objects.name1, objects.deck, objects.body, objects.rank, wires.fromid, 
-wires.toid, media.id AS mediaId, media.object, media.caption, media.type, media.active AS mediaActive FROM wires, objects 
-LEFT JOIN media ON objects.id = media.object AND media.active = 1 WHERE wires.fromid = (SELECT objects.id FROM objects WHERE 
-objects.name1 LIKE '%$search%' AND objects.active = 1) AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";
-            */
-
-            // this is the hiccup, need to get category_id here correctly
-            // passed in through the form
-            // YAY! this works
-
-            $category_id = $c['id'];
-
+            $category_0_id = $categories_search[0]['id'];
+            $category_1_id = $categories_search[1]['id'];
+            $category_2_id = $categories_search[2]['id'];
+            $category_3_id = $categories_search[3]['id'];
+            
             $sql = "SELECT objects.id AS objectsId, objects.name1, objects.deck, objects.body, objects.rank, wires.fromid, 
 wires.toid, media.id AS mediaId, media.object, media.caption, media.type, media.active AS mediaActive FROM wires, objects LEFT 
-JOIN media ON objects.id = media.object AND media.active = 1 WHERE wires.fromid = (SELECT objects.id FROM objects WHERE 
-objects.id = $category_id AND objects.active = 1) AND objects.name1 LIKE '%$search%' AND wires.toid=objects.id 
+JOIN media ON objects.id = media.object AND media.active = 1 WHERE (wires.fromid = (SELECT objects.id FROM objects WHERE 
+objects.id = $category_0_id AND objects.active = 1) OR wires.fromid = (SELECT objects.id FROM objects WHERE
+objects.id = $category_1_id AND objects.active = 1) OR wires.fromid = (SELECT objects.id FROM objects WHERE
+objects.id = $category_2_id AND objects.active = 1) OR wires.fromid = (SELECT objects.id FROM objects WHERE
+objects.id = $category_3_id AND objects.active = 1)) AND objects.name1 LIKE '%$search%' AND wires.toid=objects.id 
 AND wires.active = 1 ORDER BY objects.rank;";
 
        	    $result = MYSQL_QUERY($sql);
-            // $myrow = MYSQL_FETCH_ARRAY($result);
-            
-            } else {
+            // $myrow = MYSQL_FETCH_ARRAY($result);            
 
-            $category_id = $c['id'];    
-            // SQL objects attached to category object plus media plus rootname, rootbody
-   
-    	    $sql = "SELECT objects.id AS objectsId, objects.name1, objects.deck, objects.body, objects.rank, (SELECT 
-objects.name1 FROM objects WHERE objects.id = $category_id) AS rootname, (SELECT objects.body FROM objects WHERE objects.id =    
-$category_id) AS rootbody, wires.fromid, wires.toid, media.id AS mediaId, media.object, media.caption, media.type, media.active 
-AS mediaActive FROM wires, objects LEFT JOIN media ON objects.id = media.object AND media.active = 1 WHERE wires.fromid =     
-(SELECT objects.id FROM objects WHERE objects.id = $category_id AND objects.active = 1) AND wires.toid=objects.id AND 
-wires.active = 1 ORDER BY objects.rank;";
-    	    $result = MYSQL_QUERY($sql);
-            $myrow = MYSQL_FETCH_ARRAY($result);
-            $rootname = $myrow['rootname'];
-            $rootbody = $myrow['rootbody'];
-            mysql_data_seek($result, 0);    // reset to row 0
-            }
 	        $html = "";
             $images = [];
     	    $i=0;
@@ -155,13 +147,17 @@ wires.active = 1 ORDER BY objects.rank;";
 			        $mediaCaption = strip_tags($myrow["caption"]);
 			        $mediaStyle = "width: 100%;";
         
-	                        if ( $i == 0 ) {
+	                if ( $i == 0 ) {
         
 				        $specs  = getimagesize($mediaFile);
-				        // $use4xgrid = (($specs[0]/$specs[1]) < 1) ? TRUE : FALSE;		       
-				        $use4xgrid = ($rootname == "Buy Catalogs") ? TRUE : FALSE;		       
-	                        }
-        
+				        // $use4xgrid = (($specs[0]/$specs[1]) < 1) ? TRUE : FALSE;
+				        // true for search results   
+				        $use4xgrid = TRUE;		       
+	                }
+	                
+	                // requires category id to build url (wires.fromid)
+	                $category_id = $myrow["fromid"];
+	                
 			        $images[$i] .= "<a href='library_view_.php?id=" . $base_id . "," . $submenu_id . "," . $category_id . "," . $myrow['objectsId'] . "'>";
 			        $images[$i] .= "<div id='image".$i."' class = 'listContainer " . (($use4xgrid) ? "fourcolumn" : "twocolumn") . "'>";
 			        $images[$i] .= displayMedia($mediaFile, $mediaCaption, $mediaStyle);
@@ -177,15 +173,79 @@ wires.active = 1 ORDER BY objects.rank;";
 	        }
     
             // output $html
-                
-    	    $html .= "<div class = 'listContainer not-underlined library'>";
-            $html .= "<div class='subheadContainer library'>" . $c['name'] . "</div>";
+            // use4xgrid and doublewide
+               
+    	    $html .= "<div class = 'listContainer not-underlined library doublewide'>";
+            $html .= "<div class='subheadContainer library'>" . "Results" . "</div>";
             for ( $j = 0; $j < count($images); $j++){
                 $search_count++;
                 $html .= $images[$j];  
             }
 	        $html .= "</div>";
     	    echo nl2br($html);
+
+        } else {
+
+            foreach ($categories as $c) {
+
+                // SQL objects attached to category object plus media plus rootname, rootbody
+    
+                $category_id = $c['id'];    
+       
+        	    $sql = "SELECT objects.id AS objectsId, objects.name1, objects.deck, objects.body, objects.rank, (SELECT 
+objects.name1 FROM objects WHERE objects.id = $category_id) AS rootname, (SELECT objects.body FROM objects WHERE objects.id =    
+$category_id) AS rootbody, wires.fromid, wires.toid, media.id AS mediaId, media.object, media.caption, media.type, media.active 
+AS mediaActive FROM wires, objects LEFT JOIN media ON objects.id = media.object AND media.active = 1 WHERE wires.fromid =     
+(SELECT objects.id FROM objects WHERE objects.id = $category_id AND objects.active = 1) AND wires.toid=objects.id AND 
+wires.active = 1 ORDER BY objects.rank;";
+    	        $result = MYSQL_QUERY($sql);
+                $myrow = MYSQL_FETCH_ARRAY($result);
+                $rootname = $myrow['rootname'];
+                $rootbody = $myrow['rootbody'];
+                mysql_data_seek($result, 0);    // reset to row 0
+	            $html = "";
+                $images = [];
+    	        $i=0;
+    
+	            while ( $myrow  =  MYSQL_FETCH_ARRAY($result) ) {
+		            if ($myrow['mediaActive'] != null) {
+            
+			            $mediaFile = "MEDIA/". str_pad($myrow["mediaId"], 5, "0", STR_PAD_LEFT) .".". $myrow["type"];
+			            $mediaCaption = strip_tags($myrow["caption"]);
+			            $mediaStyle = "width: 100%;";
+            
+	                    if ( $i == 0 ) {
+            
+				            $specs  = getimagesize($mediaFile);
+				            // $use4xgrid = (($specs[0]/$specs[1]) < 1) ? TRUE : FALSE;		       
+				            $use4xgrid = ($rootname == "Buy Catalogs") ? TRUE : FALSE;		       
+	                    }
+	                    
+			            $images[$i] .= "<a href='library_view_.php?id=" . $base_id . "," . $submenu_id . "," . $category_id . "," . $myrow['objectsId'] . "'>";
+			            $images[$i] .= "<div id='image".$i."' class = 'listContainer " . (($use4xgrid) ? "fourcolumn" : "twocolumn") . "'>";
+			            $images[$i] .= displayMedia($mediaFile, $mediaCaption, $mediaStyle);
+			            $images[$i] .= "<div class = 'captionContainer library helvetica small'>";
+                        $images[$i] .= $myrow['name1'];
+			            $images[$i] .= "</div>";
+			            $images[$i] .= "</div>";
+			            $images[$i] .= "</a>";
+		            
+			            if ( ( $i+1) % (($use4xgrid) ? 4 : 2) == 0) $images[$i] .= "<div class='clear'></div>";
+             		            $i++;
+		            }
+	            }
+        
+                // output $html
+                    
+    	        $html .= "<div class = 'listContainer not-underlined library'>";
+                $html .= "<div class='subheadContainer library'>" . $c['name'] . "</div>";
+                for ( $j = 0; $j < count($images); $j++){
+                    $search_count++;
+                    $html .= $images[$j];  
+                }
+	            $html .= "</div>";
+    	        echo nl2br($html);
+    	    }
         }
     // 3/19 search position when mobile;
     if($isMobile){
