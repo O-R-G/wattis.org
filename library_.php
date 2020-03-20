dbe73da0101cd4d4e6bf28463449a58add74d5fb
david reinfurt <reinfurt@o-r-g.com>
2020-03-19 23:24:42 -0400

    refine all library_ interface, desktop and mobile

--- library_.php
+++ library_.php
@@ -27,19 +27,20 @@ require_once("GLOBAL/head.php");
             $count++;
         }
         $submenu_id = ($ids[1]) ? $ids[1] : $submenu[0]['id'];
+        $html_submenu = '<div id="library-mode-switch">';
         foreach ($submenu as $s)
             if ($s['id'] == $submenu_id)
-                $html_submenu .= '<span class = "current_submenu">' . $s['name'] . "</span><br/>";
+                $html_submenu .= '<button class = "helvetica small">' . $s['name'] . "</button>";
             else
-                $html_submenu .= "<a href='library_.php?id=" . $base_id . "," . $s['id'] . "'>" . $s['name'] ."</a><br/>";
+                $html_submenu .= "<a href='library_.php?id=" . $base_id . "," . $s['id'] . "'>" . "<button class = 'helvetica small'>" . $s['name'] ."</button></a>";
+        $html_submenu .= '</div>';
         if ($search)
-            $html_submenu = "Search: <i>$search</i><br><span id = 'search_count'></span> results.";
-        
-            
+            $html_submenu = "<br/>Search: <i>$search</i><br><span id = 'search_count'></span> results.";
+
         // build categories
 
         $sql = "SELECT objects.id, objects.name1 FROM objects, wires WHERE wires.fromid = $submenu_id AND objects.active = 1 
-AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";
+                AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";
         $result = MYSQL_QUERY($sql);
         $count = 0;
         while ($myrow = MYSQL_FETCH_ARRAY($result)) {
@@ -61,7 +62,7 @@ AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";
             $search_id = $base_id . "," . $sub_id;
             $html_search  = "<div id='library-search-container'>";
             $html_search .= "<form action='library_.php'>";
-            $html_search .= "<input id='library-search-field' type='text' placeholder='Search The Wattis Library ...' name='search'>";
+            $html_search .= "<input id='library-search-field' type='text' placeholder='SEARCH THE WATTIS LIBRARY ...' name='search'>";
             $html_search .= "<input type='hidden' id='id' name='search_id' value=$search_id>";
             $html_search .= "<button type='submit'><img id='library-search-icon' src='MEDIA/svg/magnifying-glass-6-k.svg'></button>";
             $html_search .= "</form>";
@@ -81,16 +82,14 @@ AND wires.toid=objects.id AND wires.active = 1 ORDER BY objects.rank;";
         if ($html_search)
     	    $html .= $html_search;
         $html .= "</div>";
-	    $html .= "<br />";
         if ($rootbody)
 	        $html .= $rootbody . "<br /><br />";
         if ($html_submenu){
             if(!$search){
                 // 3/19 add brief intro for library
-                $html .= "<span>This is a Library. There are videos of artists talking about their work as well as video and audio documentation of all past lectures, performances, and events. There are essays about exhibitions, as well as reviews, reading lists, and interviews to read. Browse both sections anytime.</span><br><br>";
+                $html .= "<div id='library-description'><br/>Here there are videos of artists talking about their work as well as video and audio documentation of all past lectures, performances, and events. There are also essays about exhibitions, plus reviews, reading lists, and interviews to read. The Library is organized in two sections:</div>";
             }
             $html .= $html_submenu;
-
         }
             
 	    $html .= "</div>";	
@@ -166,7 +165,7 @@ wires.active = 1 ORDER BY objects.rank;";
 			        $images[$i] .= "<a href='library_view_.php?id=" . $base_id . "," . $submenu_id . "," . $category_id . "," . $myrow['objectsId'] . "'>";
 			        $images[$i] .= "<div id='image".$i."' class = 'listContainer " . (($use4xgrid) ? "fourcolumn" : "twocolumn") . "'>";
 			        $images[$i] .= displayMedia($mediaFile, $mediaCaption, $mediaStyle);
-			        $images[$i] .= "<div class = 'captionContainer helvetica small'>";
+			        $images[$i] .= "<div class = 'captionContainer library'>";
                     $images[$i] .= $myrow['name1'];
 			        $images[$i] .= "</div>";
 			        $images[$i] .= "</div>";
@@ -179,8 +178,8 @@ wires.active = 1 ORDER BY objects.rank;";
     
             // output $html
                 
-    	    $html .= "<div class = 'listContainer not-underlined'>";
-            $html .= "<div class='subheadContainer'>" . $c['name'] . "</div>";
+    	    $html .= "<div class = 'listContainer not-underlined library'>";
+            $html .= "<div class='subheadContainer library italic '>" . $c['name'] . "</div>";
             for ( $j = 0; $j < count($images); $j++){
                 $search_count++;
                 $html .= $images[$j];  
@@ -189,8 +188,8 @@ wires.active = 1 ORDER BY objects.rank;";
     	    echo nl2br($html);
         }
     // 3/19 search position when mobile;
-    if($isMobile){?>
-        <script type = "text/javascript">
+    if($isMobile){
+        ?><script type = "text/javascript">
             var ticking = false;
             var scrollTop = window.scrollTop;
             var sLibrary_search_container = document.getElementById("library-search-container");
@@ -210,10 +209,8 @@ wires.active = 1 ORDER BY objects.rank;";
                     ticking = true;
                 }
             });
-        </script>
-
-
-    <?}
+        </script><?
+    }
     // 3/19 add search counts;
     if($search){
     ?>
@@ -221,7 +218,7 @@ wires.active = 1 ORDER BY objects.rank;";
         var sSearch_count = document.getElementById('search_count');
         sSearch_count.innerText = '<? echo $search_count; ?>';
     </script>
-
+</div>
 <?php
 }
 require_once("GLOBAL/foot.php");
