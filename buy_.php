370f0613b6b9679f0d25c1a729b26575f742a4a2
david reinfurt <reinfurt@o-r-g.com>
2020-04-17 09:54:01 -0700

    add padding to multiple images in buy_ for /catalogs

--- buy_.php
+++ buy_.php
@@ -36,7 +36,7 @@ $id AND objects.active ORDER BY media.rank;";
 
                         $mediaFile = "MEDIA/". str_pad($myrow["mediaId"], 5, "0", STR_PAD_LEFT) .".". $myrow["type"];
                         $mediaCaption = strip_tags($myrow["caption"]);
-                        $mediaStyle = "width: 100%;";
+                        $mediaStyle = "width: 100%; padding: 10px;";
 
                         if ( $i == 0 ) {
 
