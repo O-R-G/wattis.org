1aa922dcfc1e919665100414535f391941467dca
david reinfurt <reinfurt@o-r-g.com>
2020-10-29 09:01:32 -0400

    jeffrey gibson video home ready

--- video_.php
+++ video_.php
@@ -1,7 +1,7 @@
 <div id='video-home'>
     <div id='screen'>
     </div>
-    <div id='video' class='centered'>
+    <div id='video' class='center'>
         <div style="padding:56.25% 0 0 0;position:relative;">
             <iframe src="https://player.vimeo.com/video/469873476?color=FFFFFF&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
         </div>
@@ -15,10 +15,12 @@
         <div id='more' class='helvetica small'>
             Conceived during this pandemic era, the immersive video work depicts the American flag in unsettling stillness, as a marker of territory, and projected onto bodies, while set to a heartrending soundtrack. At once melancholic and beautiful, Gibson renders the iconic image of the flag as both elastic and unyielding. The slow transformation through time, color, and form reflects both a distillation of our social collapse and the reinvention of self and community, referencing the movement and change that is so desired for this nation<br />
             <br/>
-            <a href='/view?id=4,1108'>READ MORE HERE . . .</a>
-            <br/>
+            <a href='/view?id=4,1108'>READ MORE HERE</a><br/>
             <br/>
             Jeffrey Gibson (b. 1972, Colorado, US) is an interdisciplinary artist and craftsperson based in Hudson, New York. His work references various aesthetic and material histories rooted in Indigenous cultures of the Americas, and in modern and con- temporary subcultures. Gibson, a member of the Mississippi Band of Choctaw Indians and of Cherokee descent, is forging a multifarious practice that redresses the exclusion and erasure of Indigenous art traditions from the history of Western art as it explores the complexity and fluidity of identity.<br />
+            <br/>
+            <br/>
+            <br/>
         </div>
         <script src="https://player.vimeo.com/api/player.js"></script>
         <script>
@@ -27,14 +29,11 @@
             */
             var iframe = document.querySelector('iframe');
             var player = new Vimeo.Player(iframe);
-            // var screen = document.getElementById('screen');
             var body_ = document.body;
             player.on('play', function() {
-                // screen.classList.add("fade");
                 body_.classList.add("fade-bg");
             });
             player.on('pause', function() {
-                // screen.classList.remove("fade");
                 body_.classList.remove("fade-bg");
             });
         </script>
