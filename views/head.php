78b2a77bff185c9a8c98d797e819400352d22f8b
david reinfurt <reinfurt@o-r-g.com>
2021-06-24 21:37:31 -0400

    fix .logo_text_container css to match existing .logoContainer on wattis.org

--- views/head.php
+++ views/head.php
@@ -100,7 +100,7 @@ $isMobile = (bool)preg_match('#\b(ip(hone|od|ad)|android|opera m(ob|in)i|windows
 
 $bodyClass = '';
 if(!$uri[1])
-	$bodyClass .= ' home loading hiding-type';
+	$bodyClass .= ' home loading';
 if($uri[1] == 'search')
 	$bodyClass .= ' reverse';
 if($uri[1] == 'browse-the-library')
--- views/home.php
+++ views/home.php
@@ -47,7 +47,7 @@ foreach($items as $key =>$item)
             already written in views/nav so ** fix **
         */
 		// var_dump($item);
-		?><div id = 'logo_text'><?= nl2br($item["body"]); ?></div><?
+		?><div id = 'logo_text' class='logo_text_container'><?= nl2br($item["body"]); ?></div><?
 	}
 	else
 	{
